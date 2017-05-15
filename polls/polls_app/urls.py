9c2738bd340087b5052e80da89101960548f41d8
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2017-05-15 18:49:17 +0300

    add template to list polls

--- polls/polls_app/urls.py
+++ polls/polls_app/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello, name='hello'),
+    url(r'^hello/$', views.hello, name='hello'),
+    url(r'^$', views.index, name='index'),
 ]
--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -1,4 +1,8 @@
+import json
+import os
+
 from django import http
+from django.shortcuts import render
 
 
 def hello(request):
@@ -7,3 +11,12 @@ def hello(request):
         request.META['REMOTE_ADDR']
     )
     return http.HttpResponse(content, status=200)
+
+
+def index(request):
+    file_path = os.path.join(os.path.dirname(__file__), "polls.json")
+    with open(file_path) as fp:
+        polls = json.loads(fp.read())
+    return render(request, "polls_app/index.html", {
+        "polls": polls
+    })
